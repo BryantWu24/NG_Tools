13a4d52b15e207d7292ac07fa93e8389984044f6
Bryant.Wu <bryantwu1231@gmail.com>
2020-07-23 17:16:42 +0800

    [調整] HeroKu

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 
 
@@ -9,24 +8,15 @@ const path = require('path');
 const app = express();
 
 
-
 // Serve only the static files form the dist directory
 
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('./ dist /'));
 
 
-
-app.get('/*', function(req,res) {
-
-
-
-res.sendFile(path.join(__dirname+'/dist/index.html'));
-
-
+app.get('/ *', function (req, res) {
+  res.sendFile('index.html');
 });
-
-
 
 // Start the app by listening on the default Heroku port
 
