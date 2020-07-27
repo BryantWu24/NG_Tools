12c67fb1983658d937c960093197965e1e878fea
Bryant.Wu <bryantwu1231@gmail.com>
2020-07-27 16:20:40 +0800

    [新增] textResoucr 文本 [新增] login頁面輸入錯誤文字

---
+++ src/app/@core/utils/textResource.ts
@@ -0,0 +1,5 @@
+export const Text ={
+    erMsg_Account:'請輸入帳號',
+    erMsg_Password:'請輸入密碼',
+    erMsg_Ac_Pwd:'請輸入帳號密碼'
+}
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { Config } from '../../@core/utils/config'
+import { Config } from '../../@core/utils/config';
+import { Text } from '../../@core/utils/textResource';
 
 @Component({
     selector: 'jb-login',
@@ -12,7 +13,7 @@ export class LoginPageComponent implements OnInit {
 
     public accountInput;
     public passwordInput;
-    public errorMessage: string;
+    public errorMessage: string = "";
     constructor(
         private http: HttpClient,
         private router: Router
@@ -20,25 +21,42 @@ export class LoginPageComponent implements OnInit {
 
     ngOnInit(): void { }
 
-    login(id) {
+    login(id: string) {
         if (id == 'Member') {
-            const body = {
-                "userName": this.accountInput,
-                "passWord": this.passwordInput
+
+            if ((this.accountInput == "" || this.accountInput == undefined) && (this.passwordInput == "" || this.passwordInput == undefined))
+                this.errorMessage = Text.erMsg_Ac_Pwd;
+            else {
+                if (this.accountInput == "" || this.accountInput == undefined)
+                    this.errorMessage = Text.erMsg_Account;
+
+                if (this.passwordInput == "" || this.passwordInput == undefined)
+                    this.errorMessage = Text.erMsg_Password;
+
+                if ((this.accountInput != "" && this.accountInput != undefined) && (this.passwordInput != "" && this.passwordInput != undefined))
+                    this.loginApi();
             }
-            this.http.post<any>(Config.API_Root + Config.API_Login, body).subscribe((res) => {
-                if (res.status == -1) {
-                    this.errorMessage = res.data;
-                }
-                else {
-                    sessionStorage.setItem('Identity', res.data);
-                    this.router.navigate(['/pages/chat/index']);
-                    this.errorMessage = null;
-                }
-            })
         } else {
             sessionStorage.setItem('Identity', 'Guest');
             this.router.navigate(['/pages/analytics/d3']);
         }
+    }
+
+    loginApi() {
+        const body = {
+            "userName": this.accountInput,
+            "passWord": this.passwordInput
+        };
+
+        this.http.post<any>(Config.API_Root + Config.API_Login, body).subscribe((res) => {
+            if (res.status == -1) {
+                this.errorMessage = res.data;
+            }
+            else {
+                sessionStorage.setItem('Identity', res.data);
+                this.router.navigate(['/pages/chat/index']);
+                this.errorMessage = null;
+            }
+        });
     }
 }
